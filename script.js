de16cd209fd4dbb79042eb5c393bd8c7943413db
Dev-Isaque <isaquesbl123@gmail.com>
2023-10-31 01:17:23 -0300

    up

--- script.js
+++ script.js
@@ -1,42 +1,31 @@
-function updateOnScrollAndResize() {
-    var windowWidth = window.innerWidth;
-  
-    // Verifica se a largura da janela é maior que 992px
-    if (windowWidth > 992) {
-      var navbar = document.querySelector(".navbar-scroll");
-      var section = document.querySelector(".header-home");
-  
-      if (section.getBoundingClientRect().bottom <= 0) {
-        navbar.classList.add("bg-white");
-      } else {
-        navbar.classList.remove("bg-white");
-      }
+window.addEventListener("scroll", function() {
+    var navbar = document.querySelector(".navbar-scroll");
+    var section = document.querySelector(".header-home");
+
+    if (section.getBoundingClientRect().bottom <= 0) {
+      navbar.classList.add("bg-white");
     } else {
-      // Largura menor que 992px, então você pode fazer algo diferente, como deixar o background transparente
-      var navbar = document.querySelector(".navbar-scroll");
       navbar.classList.remove("bg-white");
     }
-  }
-  
-  // Chama a função de atualização quando a página carrega
-  updateOnScrollAndResize();
-  
-  // Adiciona um ouvinte de evento para atualização quando a janela for redimensionada
-  window.addEventListener("scroll", updateOnScrollAndResize);
-  window.addEventListener("resize", updateOnScrollAndResize);
+});
 
 // Menu Hamburguer
-
 $(document).ready(function() {
     // Quando a página carregar, verifique o tamanho da tela e ajuste a visibilidade
     toggleMenuVisibility();
 
-    // Quando o botão do hamburguer for clicado
     $("#btn-hamburguer").on("click", function() {
         $(".navbar-nav").slideToggle();
-        // Alternar a visibilidade dos ícones do menu e "X"
         $("#menu-icon").toggle();
         $("#close-icon").toggle();
+
+        if ($(window).width() < 995) {
+            // Aplicar o fundo branco apenas quando a largura for menor que 995 pixels
+            $(".navbar-nav").css("background-color", "#fff");
+        } else {
+            // Remover o fundo branco quando a largura for maior ou igual a 995 pixels
+            $(".navbar-nav").css("background-color", "transparent");
+        }
     });
 
     $(window).resize(function() {
